a0139970935a71e4c06bf104eb22350adb88692b
Marios Koulakis <mariosco100@gmail.com>
2016-09-20 14:45:52 -0400

    List of tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,4 +3,8 @@ module Spec where
 import Lib (test_function)
 import Test.HUnit
 
-test_test = TestCase $ assertEqual "Parsing \"100\"" 100 (test_function "100")
+test_test = TestCase $ assertEqual "Parsing \"100\"" 1001 (test_function "100")
+test1 = TestCase $ assertEqual "One more test" 101 (test_function "10")
+
+specs = TestList [TestLabel "test-test" test_test,
+                  TestLabel "test1" test1]
