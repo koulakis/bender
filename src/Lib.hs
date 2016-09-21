d47a4cd5a6696cecc558fd2964bd141ff3ae7d98
Marios Koulakis <mariosco100@gmail.com>
2016-09-21 01:07:25 -0400

    Functions that parse the map symbols and print the directions

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,6 +2,7 @@ module Lib
     ( computeBendersMoves
     ) where
 
+-- Types
 data Direction = S | E | N | W deriving (Show)
 data Block =
   Start
@@ -20,6 +21,28 @@ data Bender =
          , heading :: Direction
          , alive :: Bool
          , obstaclesEaten :: Int} deriving (Show)
+
+-- Parsing, showing
+directionName direction =
+   case direction of
+   S -> "SOUTH"
+   E -> "EAST"
+   N -> "NORTH"
+   W -> "WEST"
+readMapSymbol symbol =
+  case symbol of
+  '@' -> Start
+  ' ' -> Empty
+  '#' -> Wall
+  'X' -> Obstacle
+  '$' -> Death
+  'S' -> NewHeading S
+  'E' -> NewHeading E
+  'N' -> NewHeading N
+  'W' -> NewHeading W
+  'B' -> Beer
+  'I' -> Invert
+  'T' -> Teleporter
 
 computeBendersMoves :: Int -> Int -> [String] -> [String]
 computeBendersMoves = undefined
