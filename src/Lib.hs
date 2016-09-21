6bbbeb2a9c68f69d428b25f6b0ef7de22953c647
Marios Koulakis <mariosco100@gmail.com>
2016-09-21 00:57:40 -0400

    Types needed to define the states of Bender and the map

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,24 @@ module Lib
     ( computeBendersMoves
     ) where
 
+data Direction = S | E | N | W deriving (Show)
+data Block =
+  Start
+  | Empty
+  | Wall
+  | Obstacle
+  | Death
+  | NewHeading Direction
+  | Beer
+  | Invert
+  | Teleporter deriving (Show)
+data Bender =
+  Bender { inverted :: Bool
+         , breakerMode :: Bool
+         , location :: (Int, Int)
+         , heading :: Direction
+         , alive :: Bool
+         , obstaclesEaten :: Int} deriving (Show)
+
 computeBendersMoves :: Int -> Int -> [String] -> [String]
 computeBendersMoves = undefined
